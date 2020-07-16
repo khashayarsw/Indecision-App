a40b673f6a240421fd2ef566ae64d374429af8f4
khashayar <khashayarmahg@gmail.com>
2020-07-16 21:11:18 +0430

    Updates

---
+++ precache-manifest.f7a769dc1c80fefc4b303db4eeb6dc0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6199af9d30aca3cba57d4f2ad6a85aeb",
+    "url": "/Indecision-App/index.html"
+  },
+  {
+    "revision": "b0d9dedfe2a6f64efd7f",
+    "url": "/Indecision-App/static/css/main.5644886e.chunk.css"
+  },
+  {
+    "revision": "0b090b5ea3587b0fa8fe",
+    "url": "/Indecision-App/static/js/2.868d0a8e.chunk.js"
+  },
+  {
+    "revision": "b0d9dedfe2a6f64efd7f",
+    "url": "/Indecision-App/static/js/main.9620fc31.chunk.js"
+  },
+  {
+    "revision": "da81c93fc7546666e4cb",
+    "url": "/Indecision-App/static/js/runtime-main.be53eacb.js"
+  }
+]);
